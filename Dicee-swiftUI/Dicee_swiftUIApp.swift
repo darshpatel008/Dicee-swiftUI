e4856975505d861a4d6447f3587d313cd69382d3
darshviroja008 <darshviroja12345@gmail.com>
2024-09-18 10:36:06 +0530

    Initial Commit

---
+++ Dicee-swiftUI/Dicee_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_swiftUIApp.swift
+//  Dicee-swiftUI
+//
+//  Created by Darsh viroja  on 18/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
